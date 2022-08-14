326d908e418fb14e90598a9f9fc6f5b86356feda
avfrontend <avraam.kastrappis@gmail.com>
2022-08-14 12:28:28 +0300

    renamed file

--- src/components/Calendar/CalendarEvents.js
+++ src/components/Calendar/CalendarEvents.js
@@ -10,7 +10,7 @@ import {
   formatTime,
   formatDate,
   removeSymbolsDate,
-} from "./dateModifiers";
+} from "./DateModifiers";
 
 const url =
   "https://prod-179.westeurope.logic.azure.com/workflows/7c84997dd6894507a60796acb06e5c43/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6hFoizfo2w62d0iQK_Zyt7a3Ycr9akAkXdCPAG0ecwQ&usr=41767261616d";
