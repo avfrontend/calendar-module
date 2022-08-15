f0235a55917cb2626ebdcdb952d023fb7aceb3ea
Akis Kastrappis <kastrappis@Akiss-MacBook-Pro.local>
2022-08-15 12:46:14 +0300

    updated details for ics file

--- src/components/Calendar/saveEvent.js
+++ src/components/Calendar/saveEvent.js
@@ -1,5 +1,5 @@
 export const saveCallInvite = (props) => {
-  let url = [
+  let eventDetails = [
     "BEGIN:VCALENDAR",
     "VERSION:2.0",
     "PRODID:-//hacksw/handcal//NONSGML v1.0//EN",
@@ -11,18 +11,26 @@ export const saveCallInvite = (props) => {
     "DTSTAMP:" + props.fullCreatedDate,
     "SUMMARY:" + props.title,
     "DESCRIPTION:",
-    "LOCATION:" + props.addressLine1,
+    "LOCATION:" +
+      props.addressLine1 +
+      " " +
+      (props.addressLine2 ? props.addressLine2 + " " : "") +
+      props.city +
+      " " +
+      props.postCode +
+      " " +
+      props.country,
     "END:VEVENT",
     "END:VCALENDAR",
   ].join("\n");
 
-  let blob = new Blob([url], { type: "text/calendar;charset=utf-8" });
+  let blob = new Blob([eventDetails], { type: "text/calendar;charset=utf-8" });
 
   if (/msie\s|trident\/|edge\//i.test(window.navigator.userAgent)) {
     // Open/Save link in IE and Edge
     window.navigator.msSaveBlob(blob, `.ics`);
   } else {
     // Open/Save link in Modern Browsers
-    window.open(encodeURI("data:text/calendar;charset=utf8," + url));
+    window.open(encodeURI("data:text/calendar;charset=utf8," + eventDetails));
   }
 };
